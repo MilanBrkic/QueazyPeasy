07bd3e5484a65434eb1c49782fc18e2b04ed073c
Milica Ristic <risticmilica988@gmail.com>
2020-01-23 21:56:53 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -1,13 +1,85 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+<link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Open+Sans" />
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" type="text/css" href="css/indexstyle.css">
+    <link rel="SHORTCUT ICON" href="favicon.ico" type="image/x-icon" />
+    <link rel="ICON" href="favicon.ico" type="image/ico" /> 
     <title>QueazyPeasy</title>
 </head>
 <body>
+    <div class="centar">
+        <img src="logo/qplogowhite.png" alt="logo">
+        
+        
+    </div>
+
+    <div class="signin centar">
+        <br> Sign in to QueazyPeasy
+    </div>
     
+    <div class="container">
+        <div class="row">
+            <div class="col xl-5">
+                
+            </div>
+            <div class="col xl-2" id="box">
+                <form>
+                    Username: <br>
+                    <input size="35%" type="text" placeholder="Enter your username"> <br>
+                    Password:<br>
+                    <input size="35%" type="password" placeholder="Enter your password"><br>
+                    <input type="submit" value="Sign in " class="btn btn-primary " id="buton">
+                </form>
+            </div>
+
+
+            <div class="col xl-5">
+                
+            </div>
+        </div>
+
+        
+
+        <div class="row">
+            <div class="col xl-5">
+                
+            </div>
+            <div class="col xl-2 google">
+                <div class="google-btn">
+                    <div class="google-icon-wrapper">
+                        <img class="google-icon" src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"/>
+                    </div>
+                        <p class="btn-text"><b>Sign in with google</b></p>
+                    </div>
+            </div>
+
+
+            <div class="col xl-5">
+                
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col xl-5">
+                
+            </div>
+            <div class="col xl-2" id="newto">
+                <p>New to QueazyPeasy? <a href="">Create an account</a>.</p>  
+            </div>
+
+
+            <div class="col xl-5">
+                
+            </div>
+        </div>
+        
+        
+    </div>
+
 </body>
 </html>
